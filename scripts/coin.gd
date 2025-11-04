c0a3e8008672622baa80a828a1c518b9ca9b7444
sigma-of-the-valley <varga.andras.cserenfa@gmail.com>
2025-11-04 20:59:49 +0100

    custom coin colletcion text
    works fine and looks better, makes it more platformery

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,9 @@ extends Area2D
 @onready var game_manager: Node = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_point()
-	animation_player.play("pickup")
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.collect_coin()
+		queue_free()  # remove the coin
+		animation_player.play("pickup")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
@@ -46,3 +47,35 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+@onready var message_label: Label = $ScoreLabel
+var coins_collected: int = 0
+
+func _ready():
+	message_label.visible = false
+
+func collect_coin():
+	coins_collected += 1
+	show_floating_message("You've collected %d coins!" % coins_collected)
+
+func show_floating_message(text: String):
+	message_label.text = text
+	message_label.visible = true
+	message_label.modulate.a = 1.0  # full opacity
+	
+	# Center label horizontally above player, doesn't work with other tried solutions so this does the trick
+	message_label.position = Vector2(-message_label.size.x / 2, -40)  # just above player head
+	
+	message_label.size = message_label.get_minimum_size()
+	message_label.pivot_offset = message_label.size / 2.0
+	
+	var tween = create_tween()
+	# Float upward 20 pixels over 1.5 seconds
+	tween.tween_property(message_label, "position:y", message_label.position.y - 20, 1.5)
+	# Fade out at the same time
+	tween.tween_property(message_label, "modulate:a", 0.0, 1.5)
+
+	# When done, hide the label
+	tween.finished.connect(func():
+		message_label.visible = false
+	)
